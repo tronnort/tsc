515a33d9964f8bcb4951e3be17c1ef7031fb4629
tronnort <38342685+tronnort@users.noreply.github.com>
2019-11-20 01:00:50 +0800

    cors拦截器

--- springboot-study/src/main/java/com/tron/web/config/MyConfig.java
+++ springboot-study/src/main/java/com/tron/web/config/MyConfig.java
@@ -12,6 +12,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.http.MediaType;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -120,5 +123,32 @@ public class MyConfig {
         fastJsonHttpMessageConverter.setSupportedMediaTypes(mediaTypes);
         return new HttpMessageConverters(fastJsonHttpMessageConverter);
     }
+
+
+    /**
+     * 跨域请求放行
+     * @return
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        //1.添加CORS配置信息
+        CorsConfiguration config = new CorsConfiguration();
+        //放行哪些原始域
+        config.addAllowedOrigin("*");
+        //是否发送Cookie信息
+        config.setAllowCredentials(true);
+        //放行哪些原始域(请求方式)
+        config.addAllowedMethod("*");
+        //放行哪些原始域(头部信息)
+        config.addAllowedHeader("*");
+        //暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
+        config.addExposedHeader("*");
+        //2.添加映射路径
+        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+        configSource.registerCorsConfiguration("/**", config);
+        //3.返回新的CorsFilter.
+        return new CorsFilter(configSource);
+    }
+
 
 }
