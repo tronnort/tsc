1dfbc86cd87011ccedb2f45fbd7dd79a56253f24
tronnort <38342685+tronnort@users.noreply.github.com>
2019-11-29 00:44:16 +0800

    取环境变量的各种姿势

---
+++ tron-spring-boot-starter-autoconfigurer/src/main/java/com/tron/utils/QueryWrapperFactory.java
@@ -0,0 +1,46 @@
+package com.tron.utils;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+
+import java.util.Map;
+
+public class QueryWrapperFactory {
+    public static <T> QueryWrapper create(T t, Map<String, String> map) {
+        if (map.isEmpty()) {
+            return null;
+        }
+        QueryWrapper<T> queryWrapper = new QueryWrapper<>();
+        map.forEach((k, v) -> {
+            if (v.trim().startsWith("=")) {
+                queryWrapper.eq(false, k, v.replace("=", ""));
+            }else
+            if (v.trim().startsWith("!=")) {
+                queryWrapper.ne(false, k, v.replace("!=", ""));
+            }else
+            if (v.trim().startsWith(">")) {
+                queryWrapper.gt(false, k, v.replace(">", ""));
+            }else
+            if (v.trim().startsWith("<")) {
+                queryWrapper.lt(false, k, v.replace("<", ""));
+            }else
+            if (v.trim().startsWith("%") && v.endsWith("%")) {
+                queryWrapper.like(false, k, v.replaceAll("%", ""));
+            }else
+            if (v.trim().equals("=null")) {
+                queryWrapper.isNull(k);
+            }else
+            if (v.trim().equals("!=null")) {
+                queryWrapper.isNotNull(k);
+            }else
+            if (v.trim().startsWith("-")) {
+                queryWrapper.orderByDesc(k);
+            }else
+            if (v.trim().endsWith("+")) {
+                queryWrapper.orderByAsc(k);
+            }
+
+        });
+        return queryWrapper;
+    }
+
+}
